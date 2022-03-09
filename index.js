0e251e02fa07a3b4be2dcd78af8cecf6107f22b5
Kollin Elliott <kjelliott25@gmail.com>
2022-03-08 23:27:40 -0500

    Attempt 1 at a dice roller for a 20 sided die. Seems like something is broken.

--- index.js
+++ index.js
@@ -4,5 +4,18 @@
 let turtleButton = document.getElementById("turtleButton");
 
 turtleButton.addEventListener("click", function(e) {
-    console.log("Risked it..")
+    document.body.innerHTML = "<h1>You Risked It...</h1>"
+    rollDice()
 });
+
+if (rollDice === 1) {
+    document.body.innerHTML = 
+    "<h2>You have been turned into a turtle...</h2>";
+} else {
+    document.body.innerHTML = "<h2>You have won!! +$100,000</h2>"
+}
+function rollDice() {
+    var rollDice = 
+        math.floor(math.random() * 20) + 1;
+        return rollDice;
+}
