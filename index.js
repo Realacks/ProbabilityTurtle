711963799c1964c32807d06ac256817c8058c2df
Kollin Elliott <kjelliott25@gmail.com>
2022-03-08 22:29:43 -0500

    turtleButton event listener

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+// turtleButton code, identify button, run probability calc, output //
+
+let turtleButton = document.getElementById("turtleButton");
+
+turtleButton.addEventListener("click", function(e) {
+    console.log("Risked it..")
+});
