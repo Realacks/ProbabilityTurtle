a8a31512680a2be2ec24a78b6ccf7c608adf00e1
Kollin Elliott <kjelliott25@gmail.com>
2022-03-11 21:21:05 -0500

    Merge branch 'master' of https://github.com/Realacks/ProbabilityTurtle

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
 
 // turtleButton code, identify button, run probability calc, output //
 
-/*let turtleButton = document.getElementById("turtleButton");
-
-function rollDice() {
-    return Math.floor(Math.random() * 20) + 1)
-}
-
-
-turtleButton.addEventListener("click", function(e) {
-    document.body.innerHTML = "<h1>You Risked It...</h1>"
-    rollDice()
-    console.log (rollDice())
-});
-
- 
-if (rollDice === 1) {
-    document.body.innerHTML = 
-    "<h2>You have been turned into a turtle...</h2>";
-} else {
-    document.body.innerHTML = "<h2>You have won!! +$100,000</h2>"
-}
-function rollDice() {
-    var rollDice = 
-        math.floor(math.random() * 20) + 1;
-        return rollDice;
-}
-*/
 
 const turtleButton = document.getElementById("turtleButton");
 
@@ -39,9 +13,9 @@ function riskIt() {
     }
     const diceRoll = rollDice()
     console.log (diceRoll)
-    if (diceRoll > 10) {
-        document.body.innerHTML = "<h2>Darn.. You have been turned into a Turtle<h2>";
+    if (diceRoll === 1 ) {
+        window.location.href = "https://www.google.com/search?q=You+have+been+turned+into+a+turtle";
     } else {
         document.body.innerHTML = "<h2>Congratulations!! You have not been turned into a Turtle<h2>";
     }
-    };
+};
