e622824c0950f057875a2f4910a037080b945e07
Kollin Elliott <kjelliott@gmail.com>
2022-03-12 22:16:07 -0500

    Winner and loser pages content, added pause to rollDice function. CSS for winner/loser pages.

--- index.js
+++ index.js
@@ -7,15 +7,29 @@ const turtleButton = document.getElementById("turtleButton");
 turtleButton.addEventListener("click", riskIt);
 
 function riskIt() {
+    setTimeout (function() {
+        function rollDice() {
+            return Math.floor(Math.random() * 20) + 1
+        }
+        const diceRoll = rollDice()
+        console.log (diceRoll)
+        if (diceRoll === 1 ) {
+            window.location.href = "loser.html";
+        } else {
+            window.location.href = "winner.html";
+        }
+    }, 1000);
+
     document.body.innerHTML = "<h1>You Risked It...</h1>"
-    function rollDice() {
+/*    function rollDice() {
         return Math.floor(Math.random() * 20) + 1
     }
     const diceRoll = rollDice()
     console.log (diceRoll)
     if (diceRoll === 1 ) {
-        window.location.href = "https://www.google.com/search?q=You+have+been+turned+into+a+turtle";
+        window.location.href = "loser.html";
     } else {
-        document.body.innerHTML = "<h2>Congratulations!! You have not been turned into a Turtle<h2>";
+        window.location.href = "winner.html";
     }
+    */
 };
