9d22112839660ffdcb1e11d527a559cccaa95302
Kollin Elliott <kjelliott@gmail.com>
2022-04-06 19:46:28 -0400

    Updated js to ammend array with entries from rolls and started work on count of array entries to display streak

--- index.js
+++ index.js
@@ -6,30 +6,32 @@ const turtleButton = document.getElementById("turtleButton");
 
 turtleButton.addEventListener("click", riskIt);
 
-function riskIt() {
-    setTimeout (function() {
-        function rollDice() {
-            return Math.floor(Math.random() * 20) + 1
-        }
-        const diceRoll = rollDice()
-        console.log (diceRoll)
-        if (diceRoll === 1 ) {
-            window.location.href = "loser.html";
-        } else {
-            window.location.href = "winner.html";
-        }
-    }, 1000);
+let storedRolls = localStorage.getItem('resultArray');
 
-    document.body.innerHTML = "<h1>You Risked It...</h1>"
-/*    function rollDice() {
+let resultArray = storedRolls ? JSON.parse(storedRolls) : [];
+
+function riskIt() {
+    function rollDice() {
         return Math.floor(Math.random() * 20) + 1
     }
-    const diceRoll = rollDice()
-    console.log (diceRoll)
+    const diceRoll = rollDice();
+    console.log (diceRoll);
     if (diceRoll === 1 ) {
         window.location.href = "loser.html";
     } else {
         window.location.href = "winner.html";
+    };
+    resultArray.push(diceRoll);
+    localStorage.setItem ('resultArray', JSON.stringify(resultArray));
+    let rollCounter = resultArray.length; 
+    if (resultArray.includes(1)) {
+        resultArray = [];
+        localStorage.setItem('resultArray', JSON.stringify(resultArray));
+    } else { //Trying to log streak to webpage as count of array entries//
+        let streak = document.getElementById('streakText');
+        streak.insertAdjacentText("afterend", 'Streak:', rollCounter);
     }
-    */
+    console.log (resultArray);
+    console.log(rollCounter)
 };
+
