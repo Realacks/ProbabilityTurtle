09bf37683031c38f0fe000054fc175c03084db94
Kollin Elliott <kjelliott25@gmail.com>
2022-04-06 23:25:14 -0400

    Fixed js to show count of rolls as a streak on winner page

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ let storedRolls = localStorage.getItem('resultArray');
 
 let resultArray = storedRolls ? JSON.parse(storedRolls) : [];
 
+
+//Trying to log streak to webpage as count of array entries//
+let rollCounter = resultArray.length; 
+let streak = document.getElementById('streakText');
+streak.insertAdjacentText("afterend",rollCounter);
+
 function riskIt() {
     function rollDice() {
         return Math.floor(Math.random() * 20) + 1
@@ -23,14 +29,10 @@ function riskIt() {
     };
     resultArray.push(diceRoll);
     localStorage.setItem ('resultArray', JSON.stringify(resultArray));
-    let rollCounter = resultArray.length; 
     if (resultArray.includes(1)) {
         resultArray = [];
         localStorage.setItem('resultArray', JSON.stringify(resultArray));
-    } else { //Trying to log streak to webpage as count of array entries//
-        let streak = document.getElementById('streakText');
-        streak.insertAdjacentText("afterend", 'Streak:', rollCounter);
-    }
+    } 
     console.log (resultArray);
     console.log(rollCounter)
 };
